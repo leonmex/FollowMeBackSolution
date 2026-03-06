1b18378722a6a9da4ddaaa73e0f13e37bc96c6fc
Noel Barrera <nbgsys@hotmail.com>
2026-03-06 14:18:55 +0100

    feat: Add Var for steps follow

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -7,4 +7,7 @@ class AppConfig {
 
   /// WebRTC DataChannel Keep-alive ping interval
   static const int keepAlivePingIntervalSeconds = 20;
+
+  /// Number of previous locations to save from the client
+  static const int maxClientLocationsToSave = 5;
 }
